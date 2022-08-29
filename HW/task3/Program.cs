b3e3bb83c5942bdc0bd20d2cf3481288bceb0df3
Konstantin <konstantin.kif@mail.ru>
2022-08-29 18:10:10 +0700

    add task3

---
+++ HW/task3/Program.cs
@@ -0,0 +1,40 @@
+﻿// Сгенерировать двумерный массив со случайными числами, вывести его на экран, 
+// оформить в виде функций (пригодится в дальнейшем)
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    string input = Console.ReadLine();
+    int result = int.Parse(input);
+    return result;
+}
+
+int[,] FillArray(int numLine, int numColumns)                       // Функция создания и заполнения двухмерного массива
+{
+    int[,] matrix = new int[numLine, numColumns];
+    for (int i = 0; i < matrix.GetLength(0); i++)                   // строка
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)               // столбец
+        {
+            matrix[i, j] = i + j;
+        }
+
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)                       // Функция печати массива
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)                   // строка
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)               // столбец
+        {
+            Console.Write($"{matrix[i, j]}/t");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] array = FillArray(3, 4);
+PrintArray(array);
